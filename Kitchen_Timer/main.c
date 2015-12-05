6611047361eb6f025e6356256f0a792b0c94701b
bstrobel <strobelb69@gmail.com>
2015-12-05 10:18:49 +0100

    Automatic clock setting

--- Kitchen_Timer/main.c
+++ Kitchen_Timer/main.c
@@ -1,5 +1,5 @@
 /*
- * Mega32LED1.c
+ * main.c
  *
  * Created: 23.11.2015 16:13:00
  * Author : Bernd
@@ -8,26 +8,31 @@
 #define DIGIT_PWM_REG OCR0
 
 #include <avr/io.h>
+#include <avr/eeprom.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <stdbool.h>
 #include "sevenseg.h"
 #include "debounced_keys.h"
 
-
-#define NUM_BITS_IN_INPUT 10
-
-#define DIGIT_TIMER_OVF_VECT TIMER0_OVF_vect
-#define DIGIT_TIMER_COMP_VECT TIMER0_COMP_vect
-#define DIGIT_TIMER_INIT { \
+#define TIMER0_INIT { \
 	TCCR0 = _BV(CS01); /* CS01 -> -> 8007680Hz/256/8 -> 3910Hz */ \
 	TIFR &= ~(_BV(OCF0) | _BV(TOV0)); \
 	TCNT0 = 0; \
 	OCR0 = 0; \
 	TIMSK |= _BV(TOIE0) | _BV(OCIE0); \
 }
+#define TIMER0_TICK_COUNTER_MAX 391U
 
-#define TIMER_TICK_COUNTER_MAX 391U
+#define TIMER1_INIT { \
+}
+
+#define NUM_BITS_IN_INPUT 10
+
+#define DIGIT_TIMER_OVF_VECT TIMER0_OVF_vect
+#define DIGIT_TIMER_COMP_VECT TIMER0_COMP_vect
+#define DIGIT_TIMER_INIT TIMER0_INIT
+#define TIMER_TICK_COUNTER_MAX TIMER0_TICK_COUNTER_MAX
 
 #define TIMER_OVF_MONITOR_PORT PORTA
 #define TIMER_OVF_MONITOR_DDR DDRA
@@ -84,13 +89,18 @@ ISR(DIGIT_TIMER_COMP_VECT)
 
 int main(void)
 {
+	uint8_t osccal = eeprom_read_byte(0);
+	if (osccal != 0 || osccal != 0xFF)
+	{
+		OSCCAL = osccal;
+	}
 	set_sleep_mode(SLEEP_MODE_IDLE);
 	//sleep_bod_disable();
 	sleep_enable();
 	TIMER_OVF_MONITOR_DDR |= _BV(TIMER_OVF_MONITOR_BIT);
 	TIMER_OVF_MONITOR_PORT &= ~_BV(TIMER_OVF_MONITOR_BIT);
 	TIMER_COMP_MONITOR_DDR |= _BV(TIMER_COMP_MONITOR_BIT);
-	TIMER_COMP_MONITOR_PORT |= _BV(TIMER_COMP_MONITOR_BIT);
+	TIMER_COMP_MONITOR_PORT &= ~_BV(TIMER_COMP_MONITOR_BIT);
 	keys_init();
 	digits_init();
 	DIGIT_TIMER_INIT;
